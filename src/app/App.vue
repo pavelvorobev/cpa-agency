58144957e609bc24dde8b62549b01dc368682268
Pavel <pavel_9_5@mail.ru>
2026-03-09 18:16:40 +0100

    test(api): add temporary http client usage in App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { http } from '@/common/api/http'
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  const response = await http.fetchFull({
+    method: 'GET',
+    url: '/en/multiply',
+  })
+
+  console.log(response)
+})
+</script>
 
 <template>
   <h1>CPA AGENCY</h1>
