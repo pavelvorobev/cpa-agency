92fca13e8b05115ebd4b77521d0484947a46500a
AleksBar <nesferat@mail.ru>
2026-03-09 23:49:13 +0300

    feat(ui): Add link component

---
+++ src/common/ui/UiLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { AnchorHTMLAttributes } from 'vue';
+
+interface UiLinkProps extends /* @vue-ignore */ AnchorHTMLAttributes {
+  active?: boolean;
+}
+
+defineProps<UiLinkProps>()
+</script>
+
+<template>
+  <a class="ui-link" :class="{ 'ui-link--active': active }" v-bind="$attrs">
+    <slot />
+  </a>
+</template>
+
+<style scoped lang="scss">
+.ui-link {
+  cursor: pointer;
+}
+</style>
