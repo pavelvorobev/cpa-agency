e7263b2b1b0df23dee14f8a5d606b71b6d618dea
AleksBar <nesferat@mail.ru>
2026-03-10 23:00:57 +0300

    feat: add Header component (WIP)

---
+++ src/app/ui/layout-parts/Header.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { UiLink } from "@/common/ui";
+</script>
+
+<template>
+  <header class="app-header">
+    <div class="app-header__content container">
+        <div class="app-header__logo">Logo</div>
+        <div class="app-header__main">
+            <nav class="app-header__nav">
+                <ul>
+                    <li><UiLink href="#team" class="navigation-link">Team</UiLink></li>
+                    <li><UiLink href="#benefits" class="navigation-link">Benefits</UiLink></li>
+                    <li><UiLink href="#join" class="navigation-link">Join Us</UiLink></li>
+                </ul>
+            </nav>
+            <div class="app-header__lang">
+                <UiLink href="#team">Eng</UiLink>
+                <span class="ui-link">/</span>
+                <UiLink href="#team">Рус</UiLink>
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.navigation-link {
+    padding: 0.625rem;
+}
+
+.app-header {
+  &__logo {
+    width: 2.813rem;
+    height: 2.572rem;
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__nav {
+    ul {
+        display: flex;
+        align-items: center;
+        list-style-position: inside;
+        list-style-type: none;
+        gap: 1.25rem;
+    }
+  }
+
+  &__main {
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+  }
+}
+</style>
