c3f7b5396fad2453be204660c50eb626a72bd4d9
AleksBar <nesferat@mail.ru>
2026-03-12 21:49:39 +0300

    feat: add UiIcon component

---
+++ src/common/ui/UiIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+interface Props {
+  name: string
+  color?: string
+  size?: number | string // number value in rem
+  width?: number | string // number value in rem
+  height?: number | string // number value in rem
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 20,
+  width: undefined,
+  height: undefined,
+  color: undefined,
+})
+</script>
+
+<template>
+  <svg 
+    aria-hidden="true"
+    :style="{
+      width: `${width ?? size}rem`,
+      height: `${height ?? size}rem`,
+      fill: color ?? 'currentColor'
+    }"
+  >
+    <use :href="`#icon-${props.name}`" />
+  </svg>
+</template>
+
