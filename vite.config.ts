47641a9902bc75b7e6568911891795ec36e30d17
AleksBar <nesferat@mail.ru>
2026-03-12 21:48:49 +0300

    config: add settings for vite-plugin-svg-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const getPath = (path: string) => fileURLToPath(new URL(path, import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    createSvgIconsPlugin({
+      iconDirs: [getPath('./src/app/assets/icons')],
+      symbolId: 'icon-[name]',
+      svgoOptions: {
+        plugins: [
+          {
+            name: 'removeAttrs',
+            params: { attrs: 'fill' },
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': getPath('./src')
     },
   },
   css: {
