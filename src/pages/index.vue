eb4dc73531b62e38b4648bc0a17cbf1c058b44f2
sttshka <sttshka.dev@gmail.com>
2026-03-14 15:34:52 +0300

    chore: setup theme variables with custom values from figma

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -21,9 +21,23 @@ onMounted(async () => {
   <h1>Home Page</h1>
 </template>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+@use '@/app/assets/styles/variables.scss' as vars;
+
 h1 {
-  font-size: 20rem;
-  color: $color-main;
+  color: vars.$color-yellow;
+
+  // При желании переопределить дефолтные значения тэгов аля h1 и тд
+  //@include vars.text-style(
+  //    vars.$typo-h1-size,
+  //    vars.$typo-h1-line-height,
+  //    vars.$typo-h1-weight,
+  //    vars.$typo-h1-letter-spacing
+  //);
+
+  // или передавать значение шрифта напрямую
+  //font-size: vars.fs(14);
+  //font-weight: vars.vars.$font-weight-bold;
+  //line-height: vars.tracking-in-px(-2) / vars.tracking-in-percent(10)
 }
 </style>
