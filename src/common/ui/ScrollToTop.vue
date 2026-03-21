70ff397eeb89582e1d590f2a9bdd6d8f121e2eef
sttshka <sttshka.dev@gmail.com>
2026-03-21 13:26:59 +0300

    feat(ui): add ScrollToTop component

---
+++ src/common/ui/ScrollToTop.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'ScrollToTop',
+})
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+</script>
+
+<template>
+  <button type="button" v-bind="$attrs" @click="scrollToTop">
+    <slot />
+  </button>
+</template>
--- src/common/ui/index.ts
+++ src/common/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as UiContainer } from './UiContainer.vue'
 export { default as UiIcon } from './UiIcon.vue'
+export { default as ScrollToTop } from './ScrollToTop.vue'
 export * from './buttons'
